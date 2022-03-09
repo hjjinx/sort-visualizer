5c8696f188049ed074b4514a40a91a395097afb5
Himanshu Jain <hjmanojain.8@gmail.com>
2022-03-09 11:57:33 +0530

    generated random array

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const LENGTH = 100;
 
 function App() {
+  const [arr, setArr] = useState<number[]>();
+  const genUnsortedArray = () => {
+    setArr(
+      Array.from(
+        {
+          length: LENGTH,
+        },
+        () => Math.floor(Math.random() * 1000)
+      )
+    );
+  };
+  console.log({ arr });
+  useEffect(genUnsortedArray, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="main"></main>
     </div>
   );
 }
